0f520e420258dd376cc6f31e776efc17b6649394
smahapatra86 <smahapatra86@gmail.com>
2021-05-06 10:56:29 +0530

    Remove unwanted files and fixing warnings.

--- sample/Kmd.Logic.FileSecurity.Client.ConfigurationSample/Program.cs
+++ sample/Kmd.Logic.FileSecurity.Client.ConfigurationSample/Program.cs
@@ -2,8 +2,15 @@
 
 namespace Kmd.Logic.FileSecurity.Client.ConfigurationSample
 {
+    /// <summary>
+    /// Sample class to use the client.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main method to start the sample.
+        /// </summary>
+        /// <param name="args">Array of arguments.</param>
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
